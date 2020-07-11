06540725c437a90e2fe3d69e7586b118216eedf4
kadajko <ngockhanhnguyenn@gmail.com>
2020-07-11 09:05:09 +0700

    feat (order): remove entites

--- fruitst/entities/order.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-from ..entities.order_item import OrderItem
-
-
-class Order:
-    def __init__(self, date, fruits: List[OrderItem]):
-        self.date = date
-        self.fruits = fruits
--- fruitst/entities/order_item.py
+++
@@ -1,5 +0,0 @@
-
-class OrderItem:
-    def __init__(self, fruit_name, amount):
-        self.fruit_name = fruit_name
-        self.amount = amount
